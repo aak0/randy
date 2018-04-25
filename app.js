c3e4fda734c72f6f5bac26f49b6b37d18f532b34
Aleksandr Korotkikh <code@aak.io>
2018-04-25 13:58:45 +0300

    Add GitHub auth

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const helmet = require("helmet")
+const helmet = require("helmet");
+const passport = require("passport");
+const OAuth2Strategy = require("passport-oauth2").Strategy;
 
 app.use(helmet())
 app.use(express.static(path.join(__dirname, "public")));
+
+passport.use(new OAuth2Strategy({
+  authorizationURL: "https://github.com/login/oauth/authorize",
+  tokenURL: "https://github.com/login/oauth/access_token",
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: `${process.env.URL}/auth/example/callback`
+},
+(accessToken, refreshToken, profile, cb) => {
+  User.findOrCreate({ exampleId: profile.id }, (err, user) => {
+    return cb(err, user);
+  });
+}
+));
+
+app.get("/login",
+  passport.authenticate("oauth2"));
 
 app.listen(process.env.PORT,
   () => console.log(`Example app listening on port ${process.env.PORT}`));
