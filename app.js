f1f397725a1e5388ee6bb9ecf8e1ed70c603e860
Aleksandr Korotkikh <code@aak.io>
2018-04-26 21:53:50 +0300

    Save users by GitHub ID

--- app.js
+++ app.js
@@ -8,46 +8,64 @@ const crypto = require("crypto");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
+const request = require("request");
 
 app.use(helmet())
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
-app.use(bodyParser());
+//app.use(bodyParser());
 app.use(session({ secret: process.env.SESSION_SECRET }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-let users = new Map();
+let users = {};
 
 passport.use(new OAuth2Strategy({
   authorizationURL: "https://github.com/login/oauth/authorize",
   tokenURL: "https://github.com/login/oauth/access_token",
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: `${process.env.URL}login/callback`
+  callbackURL: `${process.env.URL}/login/callback`
 },
 (accessToken, refreshToken, profile, cb) => {
-  const hash = crypto.createHash("sha256").update(accessToken).digest("base64");
-  console.log(hash);
-  if (typeof users[hash] !== "undefined") {
-    console.log("stored token", users[hash]);
-    return cb(null, users[hash]);
-  } else {
-    users[hash] = accessToken;
-    return cb(null, accessToken);
-  }
+  request({
+      url: `https://api.github.com/user?access_token=${accessToken}`,
+      headers: {
+        "User-Agent": "request"
+      }
+    },
+    (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        console.log("got something")
+        const id = JSON.parse(body).id;
+        const provider = "github";
+        // Normalise the id to make changing / adding providers easier
+        const idHash = crypto.createHash("sha256").update(`${provider}/${id}`).digest("hex");
+  
+        if (!users[idHash]) {
+          console.log("No user with hash", idHash)
+          users[idHash] = { accessToken, id, provider };
+          return cb({ accessToken, id, provider });
+        } else {
+          console.log("Found user", JSON.stringify(users[idHash]));
+          return cb(users[idHash]);
+        }
+      }
+    });
 }
 ));
 
-passport.serializeUser((accessToken, cb) => {
-  return cb(null, crypto.createHash("sha256").update(accessToken).digest("base64"));
+passport.serializeUser((user, cb) => {
+  console.log("serialising", JSON.stringify(user));
+  return cb(null, crypto.createHash("sha256").update(`${user.provider}/${user.id}`).digest("hex"));
 });
 
-passport.deserializeUser((hash, cb) => {
-  if (typeof users[hash] !== "undefined") {
-    return cb(null, users[hash]);
+passport.deserializeUser((idHash, cb) => {
+  console.log("deserialising", JSON.stringify(idHash));
+  if (!users[idHash]) {
+    return cb(new Error('hello'));
   } else {
-    return cb(new Error());
+    return cb(null, users[idHash]);
   }
 });
 
@@ -63,6 +81,7 @@ app.get("/login/callback",
   passport.authenticate("oauth2", { failureRedirect: "/login" }),
   (req, res) => {
     // Successful authentication, redirect home.
+    console.log(req.session);
     res.redirect("/");
 });
 
