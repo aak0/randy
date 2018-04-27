ccb3cb529481cbb63091a89fbdcad94f2f16ff81
Aleksandr Korotkikh <code@aak.io>
2018-04-27 15:42:35 +0300

    Enable secure cookies

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
   resave: true,
   saveUninitialized: false, // Do not store cookies for guests
   cookie: {
-    secure: false,
+    secure: true,
     maxAge: 1000 * 60 * 60 * 72, // 72 hours
   },
   store: new NedbStore({
